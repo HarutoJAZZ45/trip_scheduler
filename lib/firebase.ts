a1318f58af700366f1ebe07a67e00594ec30119b
Haruto <hj45.git.fb@gmail.com>
2026-02-07 10:14:15 +0900

    Security: Move Firebase credentials to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD5S7XtTnpPRtmHWtUiiHxMaAs0cl22bZQ",
-    authDomain: "trip-scheduler45.firebaseapp.com",
-    projectId: "trip-scheduler45",
-    storageBucket: "trip-scheduler45.firebasestorage.app",
-    messagingSenderId: "789144108322",
-    appId: "1:789144108322:web:4c279ebcbd8ad01ba6b97b",
-    measurementId: "G-L7760JNBY6"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
